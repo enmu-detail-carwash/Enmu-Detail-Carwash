d5ec199150e5aef3a4c6bf4b857a41e243227c58
Khasiano <NewbieCoder10>
2022-11-07 17:06:31 -0700

    Updated the Employee Class.

--- Project Stage 3/sourceFiles/Employee.java
+++ Project Stage 3/sourceFiles/Employee.java
@@ -1,2 +1,79 @@
 public class Employee {
+
+  private int ID; // = defaultValue;
+  private int Age; // - defaultValue;
+  private String Name; // = defaultValue;
+  private double WeeklySalary; // = defaultValue;
+  private int OThrs; // = defaultValue;
+  private double OTrate = 26.25;
+  private boolean PayStatus = false;
+
+  public Employee() {
+
+  }
+
+  public Employee(int ID, int Age, String name) {
+    this.ID = ID;
+    this.Age = Age;
+    this.name = name;
+
+  }
+
+  public Employee(int ID, int Age, String name, double WeeklySalary) {
+    this.ID = ID;
+    this.Age = Age;
+    this.name = name;
+    this.WeeklySalary = WeeklySalary;
+
+  }
+
+  public String getName() {
+    return Name;
+  }
+
+  public void setName(String name) {
+    this.Name() = Name;
+  }
+
+  public int getAge() {
+    return Age;
+  }
+
+  public void setAge(int Age) {
+    this.Age = Age;
+  }
+
+  public int getID() {
+    return ID;
+  }
+
+  public void setID(int ID) {
+    this.ID = ID;
+  }
+
+  public void setWeeklySalary(double WeeklySalary) {
+    this.WeeklySalary = WeeklySalary;
+  }
+
+  public void setOThrs(int OThrs) {
+    this.OThrs = OThrs;
+  }
+
+  public int getOThrs() {
+    return OThrs;
+  }
+
+  public int getOTpay() {
+    return OTpay;
+  }
+
+  public double getTotalPay() {
+    return TotalPay
+  }
+
+  public void paid() {
+
+  }
+
+
 }
